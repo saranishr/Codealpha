f27df32085f3e9cd9d320591f2c9b539480a2fbf
saranishr <142418625+saranishr@users.noreply.github.com>
2025-06-11 20:00:10 +0530

    Create Room.java

---
+++ Task2/Room.java
@@ -0,0 +1,25 @@
+import java.io.Serializable;
+
+public class Room implements Serializable {
+    private int roomNumber;
+    private String category;
+    private boolean isBooked;
+
+    public Room(int roomNumber, String category) {
+        this.roomNumber = roomNumber;
+        this.category = category;
+        this.isBooked = false;
+    }
+
+    public int getRoomNumber() { return roomNumber; }
+    public String getCategory() { return category; }
+    public boolean isBooked() { return isBooked; }
+
+    public void book() { this.isBooked = true; }
+    public void cancel() { this.isBooked = false; }
+
+    @Override
+    public String toString() {
+        return "Room " + roomNumber + " [" + category + "] - " + (isBooked ? "Booked" : "Available");
+    }
+}
